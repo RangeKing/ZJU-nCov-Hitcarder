17f4f5a76fc03af4b3e3c8b566ee8edfb5539a77
RangeKing <rangekinghz@gmail.com>
2022-05-08 20:33:49 +0800

    Update hitcarder.py
    add OCR API to recognize verify-code

--- hitcarder.py
+++ hitcarder.py
@@ -3,8 +3,10 @@ import requests
 from requests.adapters import HTTPAdapter
 try:
     from requests.packages.urllib3.util.retry import Retry
+    from urllib2 import URLError
 except ImportError:
     from urllib3.util import Retry
+    from urllib.error import URLError
 import json
 import re
 import time
@@ -12,7 +14,23 @@ import datetime
 import os
 import sys
 import message
+import base64
 
+
+def ocr_api(image_base64, API_KEY, SECRET_KEY):
+    '''
+    调用百度云OCR API进行验证码识别
+    '''
+    request_url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic' #使用通用文字识别（标准版）
+    host = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={API_KEY}&client_secret={SECRET_KEY}'
+
+    if response := requests.get(host):
+        access_token = response.json()['access_token']
+    params = {"image":image_base64}
+    request_url = f"{request_url}?access_token={access_token}"
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    if response := requests.post(request_url, data=params, headers=headers):
+        return response.json()
 
 class HitCarder(object):
     """Hit carder class
@@ -20,18 +38,23 @@ class HitCarder(object):
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
+        API_KEY: (str) 百度智能云 API_KEY
+        SECRET_KEY: (str) 百度智能云 SECRET_KEY
         login_url: (str) 登录url
         base_url: (str) 打卡首页url
         save_url: (str) 提交打卡url
         sess: (requests.Session) 统一的session
     """
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, API_KEY, SECRET_KEY):
         self.username = username
         self.password = password
+        self.API_KEY = API_KEY
+        self.SECRET_KEY = SECRET_KEY
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.code_url = "https://healthreport.zju.edu.cn/ncov/wap/default/code"
         self.sess = requests.Session()
         self.sess.keep_alive = False
         retry = Retry(connect=3, backoff_factor=0.5)
@@ -100,6 +123,16 @@ class HitCarder(object):
         #     f.write(new_form)
         return False
 
+    def get_verify_code(self):
+        """Get verify code"""
+        res = self.sess.get(self.code_url)
+        code_base64 = base64.b64encode(res.content)
+        try:
+            verify_code = ocr_api(code_base64, self.API_KEY, self.SECRET_KEY)['words_result'][0]['words']
+        except URLError as err:
+            print(err)
+        return verify_code
+
     def get_info(self, html=None):
         """Get hit card info, which is the old info with updated new time."""
         if not html:
@@ -128,6 +161,7 @@ class HitCarder(object):
 
         except json.decoder.JSONDecodeError as err:
             raise DecodeError(f'JSON decode error: {str(err)}') from err
+
         new_info = def_info.copy()
         new_info.update(magic_code_group)
         # form change
@@ -141,6 +175,7 @@ class HitCarder(object):
         new_info['sfzx'] = old_info['sfzx']  # 在校
         new_info['sfymqjczrj'] = old_info['sfymqjczrj']  # 入境
         new_info['sfqrxxss'] = 1  # 属实
+        new_info['verifyCode'] = self.get_verify_code() # 验证码
 
         self.info = new_info
         # print(json.dumps(self.info))
@@ -171,7 +206,7 @@ class DecodeError(Exception):
     pass
 
 
-def main(username, password):
+def main(username, password, API_KEY, SECRET_KEY):
     """Hit card process
 
     Arguments:
@@ -179,7 +214,7 @@ def main(username, password):
         password: (str) 浙大统一认证平台密码
     """
 
-    hit_carder = HitCarder(username, password)
+    hit_carder = HitCarder(username, password, API_KEY, SECRET_KEY)
     print("[Time] %s" % datetime.datetime.now().strftime(
         '%Y-%m-%d %H:%M:%S'))
     print(datetime.datetime.now(datetime.timezone.utc) +
@@ -220,12 +255,14 @@ def main(username, password):
 if __name__ == "__main__":
     username = os.environ['USERNAME']
     password = os.environ['PASSWORD']
+    API_KEY = os.environ['API_KEY']
+    SECRET_KEY = os.environ['SECRET_KEY']
 
-    ret, msg = main(username, password)
+    ret, msg = main(username, password, API_KEY, SECRET_KEY)
     print(ret, msg)
     if ret == 1:
         time.sleep(5)
-        ret, msg = main(username, password)
+        ret, msg = main(username, password, API_KEY, SECRET_KEY)
         print(ret, msg)
 
     if dingtalk_token := os.environ.get('DINGTALK_TOKEN'):
@@ -237,5 +274,5 @@ if __name__ == "__main__":
         print('send_serverChan_message', ret)
 
     if pushplus_token := os.environ.get('PUSHPLUS_TOKEN'):
-        print('pushplus服务已下线，建议使用钉钉')
+        print('pushplus服务已下线, 建议使用钉钉')
         exit(-1)
